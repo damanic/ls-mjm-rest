d5237c8b8b2257cfab1aed0c8085ac85ddd8df0b
Matt <matt@mjman.net>
2014-12-07 12:59:12 +1100

    readme

--- modules/mjmrestful/classes/mjmrestful_module.php
+++ modules/mjmrestful/classes/mjmrestful_module.php
@@ -1,28 +1,4 @@
 <?
-/* @TODO
- *  - Allow to set header OPTIONS on router __construct
- *  - Auto add routes to header OPTIONS
- *  - Allow set config on router run()
- *  - Allow set config on route add()
- *  - Force HTTPS on authentication routes by default
- *  - Allow caching
- *  - Clean up trigger to delete old keys
- *  - Set up a Config page to set:
- *      - API TOKEN NAME
- *      - API TOKEN EXPIRY IN DAYS
- *      - LOCK TOKEN TO DEVICE+IP  ON/OFF
- *      - force_correct_request_type  !!!! REDUNDANT SEE  = !*!&!
- *      - force HTTPS for authentication ON/OFF
- *  = !*!&! A path must be able to take combination of PUT/GET/POST/DELETE
- *          and route correctly based on detected request type.
- *  - GET parameters Could be supported eg.  /products/?q=search_query
- *  - Logout / Delete Token Route. Reserved
- *  - MjmRestful_Authenticate is extended by MjmRestful_AuthCustomer and MjmRestful_AuthAdmin
- *  - MjmRestful_Router takes and extension of MjmRestful_Authenticate, defaults to MjmRestful_AuthCustomer
- *  -
- *
- *
- */
 
 	class MjmRestful_Module extends Core_ModuleBase {
 
@@ -46,34 +22,5 @@
                 )
             );
         }
-
-        /*
- * Access points
- */
-        public function register_access_points(){
-            return array(
-                'restful_api'=>'api'
-            );
-        }
-
-        /**
-         * REST API
-         */
-        public function api($url_params){
-
-            //load the restful router
-            $router = new MjmRestful_Router();
-
-
-            //add Location Resource API to Router
-            $api = new MjmRestful_Location_API($router);
-
-            //we runnin json
-            $run_options = array('format'=>'json');
-
-            //RUN API APP
-            $router->run($url_params,$run_options);
-        }
-
 
     }
--- modules/mjmrestful/readme.md
+++ modules/mjmrestful/readme.md
@@ -1,1 +1,30 @@
 A module that enables simple RESTful API modules.
+
+The modules in this repository can be used for trial and development purposes, if you find it useful please install via the lemonstand market place for support and stable updates.
+
+Code contributions are welcome.
+
+
+/* @TODO
+ *  - Allow to set header OPTIONS on router __construct
+ *  - Auto add routes to header OPTIONS
+ *  - Allow set config on router run()
+ *  - Allow set config on route add()
+ *  - Force HTTPS on authentication routes by default
+ *  - Allow caching
+ *  - Clean up trigger to delete old keys
+ *  - Set up a Config page to set:
+ *      - API TOKEN NAME
+ *      - API TOKEN EXPIRY IN DAYS
+ *      - LOCK TOKEN TO DEVICE+IP  ON/OFF
+ *      - force_correct_request_type  !!!! REDUNDANT SEE  = !*!&!
+ *      - force HTTPS for authentication ON/OFF
+ *  = !*!&! A path must be able to take combination of PUT/GET/POST/DELETE
+ *          and route correctly based on detected request type.
+ *  - GET parameters Could be supported eg.  /products/?q=search_query
+ *  - Logout / Delete Token Route. Reserved
+ *  - MjmRestful_Authenticate is extended by MjmRestful_AuthCustomer and MjmRestful_AuthAdmin
+ *  - MjmRestful_Router takes and extension of MjmRestful_Authenticate, defaults to MjmRestful_AuthCustomer
+ *
+ *
+ */
